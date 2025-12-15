555185d3ca65b3490981d9e54c72650ad29f2743
Víctor Moya <prestigewebsolutions7@gmail.com>
2025-12-15 18:32:28 +0100

    v1.7.2

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@
 import tailwindcss from '@tailwindcss/vite';
 import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: vercel(),
   vite: {
       plugins: [tailwindcss()],
 	},
